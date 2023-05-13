d603a89bc30b90f41a5344b32ce039cb2db91017
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-14 01:37:24 +0530

    slugname error

--- app/api/auth/[...nextAuth]/route.js
+++ app/api/auth/[...nextAuth]/route.js
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";    
 
+import User from "@/models/user";
 
+import { connectToDatabase } from "@/utils/database";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -10,10 +12,33 @@ const handler = NextAuth({
         }),
     ],
     async session({session}) {
+        const sessionUser = await User.findOne({ email: session.user.email });
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
 
     },
     async signIn({ profile}) {
+        try{ 
+            await connectToDatabase();
+            //check if user exists already
+            const userExists = await User.findOne({ email: profile.email });
 
+            //if not, create user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.image,
+                })
+            }
+
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
     },
 });
 
